50168f3121b859c69a96f5827362bbc7768ea637
bb912 <brettbissey@gmail.com>
2019-11-04 10:42:39 -0500

    edit to NAbatchNormalrelu to save preprocessed images to directory

--- simpleNets/NAbatchNormalrelu.py
+++ simpleNets/NAbatchNormalrelu.py
@@ -28,9 +28,9 @@ def lr_schedule(epoch):
         lrate = 0.0003        
     return lrate
 
-board = keras.callbacks.TensorBoard(log_dir='./logs/normalizedModel2', 
+board = keras.callbacks.TensorBoard(log_dir='./logs/normalizedModel20191104', 
 	histogram_freq=0, 
-	batch_size=50, 
+	batch_size=32, 
 	write_graph=True)
 
 
@@ -39,24 +39,24 @@ weight_decay = 1e-4
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay), input_shape=(225, 225, 3))) #new
+model.add(Conv2D(16, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay), input_shape=(500, 500, 3))) #new
 model.add(Activation('relu'))
 model.add(BatchNormalization()) #new
 #model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.4))
+
 model.add(Conv2D(32, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay)))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(64, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay)))
-model.add(Activation('relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(64, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay)))
-model.add(Activation('elu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.3))
+#model.add(Conv2D(64, (3, 3), padding = 'same', kernel_regularizer = regularizers.l2(weight_decay)))
+#model.add(Activation('relu'))
+#model.add(BatchNormalization())
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
 
 #model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
 #model.add(Activation('elu'))
@@ -76,20 +76,20 @@ model.summary()
 
 
 datagen = ImageDataGenerator(rescale = 1./255,
-	rotation_range=40, 
-	width_shift_range=0.2, 
-	height_shift_range = 0.2, 
-	shear_range = 0.2, 
-	zoom_range = 0.2, 
-	horizontal_flip = True, 
-	fill_mode = 'nearest')
+	rotation_range=15,
+	width_shift_range=0.1, 
+	height_shift_range = 0.1,  
+	zoom_range = 0.2,  
+	fill_mode = 'nearest',
+	samplewise_std_normalization = True,
+	zca_whitening = True)
 
 
 train_datagen = datagen
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-batch_size = 50 #change this from 16
+batch_size = 32 #change this from 16
 
 opt_rms = keras.optimizers.rmsprop(lr=0.001,decay=1e-6)
 
@@ -102,29 +102,35 @@ model.compile(loss = 'categorical_crossentropy',
 
 
 train_generator = train_datagen.flow_from_directory('/home/brett/BigData/trainSet', 
-	target_size = (225, 225), 
+	target_size = (500, 500), 
 	batch_size = batch_size, 
 	class_mode = 'categorical', 
 	color_mode='rgb',
-	interpolation = 'nearest')
+	interpolation = 'nearest', 
+	save_to_dir='/home/brett/BigData/visualTrain',
+	save_format='jpeg')
 
 
 v_generator = test_datagen.flow_from_directory('/home/brett/BigData/testSet', 
-	target_size = (225,225), 
+	target_size = (500,500), 
 	batch_size = batch_size, 
 	class_mode = 'categorical', 
 	color_mode='rgb',
 	interpolation = 'nearest')
 
+#bb: steps per epoch updated. 
+
+numTrainImages = 6337*3
+numTestImages = 704*3
 
 model.fit_generator(train_generator, 
-	steps_per_epoch = 2500 // batch_size, 
-	epochs = 125, 
+	steps_per_epoch = numTrainImages // batch_size, 
+	epochs = 100, 
 	validation_data =v_generator, 
-	validation_steps = 1250 // batch_size, 
+	validation_steps = numTestImages // batch_size, 
 	callbacks = [board, LearningRateScheduler(lr_schedule)],
 	use_multiprocessing = True,
-	workers = 6
+	workers = 14
 	)
-model.save_weights('normalizedModel.h5')
+model.save_weights('normalizedModel20191104.h5')
 
